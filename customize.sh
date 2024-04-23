963aa7443305793abf80866181c6af49c375a3ec
Chelal233 <296050842@qq.com>
2024-04-24 01:07:41 +0800

    个人需求

--- customize.sh
+++ customize.sh
@@ -11,5 +11,9 @@ sed -i 's/10.0.0.101/10.0.0.1/g' openwrt/package/base-files/files/bin/config_gen
 #2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
+# 修改luci-app-aliyundrive-webdav
+rm -rf feeds/luci/applications/luci-app-aliyundrive-webdav
+rm -rf feeds/packages/multimedia/aliyundrive-webdav
+
 #3. Replace with JerryKuKu’s Argon
 #rm openwrt/package/lean/luci-theme-argon -rf
