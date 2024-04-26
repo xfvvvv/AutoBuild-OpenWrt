5128caa8fc4d1fbe7d591bbff7d82a6b425b9b4f
Chelal <31129985+xfvvvv@users.noreply.github.com>
2024-04-27 00:20:17 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -12,27 +12,15 @@ sed -i 's/192.168.1.1/10.0.0.101/g' package/base-files/files/bin/config_generate
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
 
 #3. Replace with JerryKuKu’s Argon
-rm -rf package/lean/autosamba
-
-rm -rf feeds/packages/net/mosdns
-rm -rf feeds/packages/net/msd_lite
-rm -rf feeds/packages/net/smartdns
-rm -rf feeds/packages/net/samba4
-rm -rf feeds/packages/net/vsftpd
-rm -rf feeds/packages/backuppc
 rm -rf feeds/packages/multimedia/aliyundrive-webdav
+rm -rf feeds/luci/applications/luci-app-aliyundrive-webdav
+
+rm -rf feeds/luci/applications/luci-app-mosdns
+
+rm -rf feeds/luci/luci-app-argon-config
+rm -rf feeds/luci/applications/luci-app-argon-config
 
 rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/luci/themes/luci-theme-netgear
-rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/luci/applications/luci-app-netdata
-rm -rf feeds/luci/applications/luci-app-serverchan
-rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/luci/applications/luci-app-samba
-rm -rf feeds/luci/applications/luci-app-samba4
-rm -rf feeds/luci/applications/luci-app-vsftpd
-rm -rf feeds/luci/applications/luci-app-aliyundrive-webdav
-rm -rf feeds/luci/luci-app-smartdns
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
