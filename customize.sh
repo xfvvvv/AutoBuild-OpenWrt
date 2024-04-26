00e492bceb7efd2f76e530bd4f64ec2b05f01b23
Chelal <31129985+xfvvvv@users.noreply.github.com>
2024-04-26 23:23:58 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -20,7 +20,7 @@ rm -rf feeds/packages/net/msd_lite
 rm -rf feeds/packages/net/smartdns
 rm -rf feeds/packages/net/samba4
 rm -rf feeds/packages/net/vsftpd
-rm -rf feeds/
+rm -rf package/lean/autosamba
 
 rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/luci/themes/luci-theme-netgear
