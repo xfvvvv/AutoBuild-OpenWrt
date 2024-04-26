27677dc25a97f0e216c5b7b63f9b4b62e6685b24
Chelal <31129985+xfvvvv@users.noreply.github.com>
2024-04-26 17:07:27 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,7 +6,7 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.3.122/192.168.3.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.3.1/192.168.3.122/g' package/base-files/files/bin/config_generate
 
 #2. Clear the login password
 #sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
